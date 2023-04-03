01922ab5c175c7c645e7f011e4ebb4251ee235a1
Alexey Versal <zhirenkoalexey1999@gmail.ru>
2023-04-03 10:37:03 +0300

    Test1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Constructor;
+
+Prom program = new Prom("Alex", 24);
+program.Show();
+
+class Prom
+{
+    string _name;
+    int _age;
+    public Prom(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+    public void Show()
+    {
+        Console.WriteLine($"Имя: {_name} Возраст: {_age}");
+    }
+}
+
+
---
+++ Stock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Stock
+    {
+        public delegate void PriceChange(int price);
+        public PriceChange priceChange { get; set; }
+
+        public void Start()
+        {
+            while (true)
+            {
+                int priceBank = (new Random()).Next(100);
+
+                
+            }
+        }
+    }
+
+}
